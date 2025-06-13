1fcac321bf46f3e8b0babf077073b3de7d463361
davidve0206 <davidve0206@gmail.com>
2025-06-13 19:57:56 +0100

    feat: create basic configuration folder

---
+++ src/configuration/logger.py
@@ -0,0 +1,20 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from src.configuration.settings import BASE_DIR
+
+# Log file path
+LOG_DIR = BASE_DIR / "logs"
+LOG_DIR.mkdir(exist_ok=True)
+log_file = LOG_DIR / "app.log"
+
+# Configure logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    handlers=[
+        RotatingFileHandler(log_file, maxBytes=5_000_000, backupCount=3),
+        logging.StreamHandler(),  # Optional: also log to console
+    ],
+)
+
+logger = logging.getLogger(__name__)
---
+++ src/configuration/settings.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings
+from pydantic import EmailStr, ConfigDict
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+class Settings(BaseSettings):
+    email_from_address: EmailStr = "test@test.com"
+    email_username: str | None
+    email_password: str | None
+    email_host: str = "smtp.gmail.com"
+    email_port: int = 465
+    email_use_ssl: bool = False
+    email_recipient: EmailStr = "test@test.com"
+
+    model_config = ConfigDict(extra="ignore")
+
+
+CONFIG_FILE_PATH = BASE_DIR / "src" / "configuration" / "config.env"
+app_settings = Settings(_env_file=CONFIG_FILE_PATH)
