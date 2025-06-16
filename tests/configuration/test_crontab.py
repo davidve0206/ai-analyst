0ac00fe0a8a630925f6d012d1b7f585ed80a9e75
davidve0206 <davidve0206@gmail.com>
2025-06-16 19:17:31 +0100

    chore: update tests and expand to ensure order of user inputs is irrelevant

--- tests/configuration/test_crontab.py
+++ tests/configuration/test_crontab.py
@@ -1,6 +1,6 @@
 from src.configuration.crontab import (
     CrontabFrequency,
-    CronFrequency,
+    JobFrequency,
     Weekday,
     Month,
     set_crontab,
@@ -13,7 +13,20 @@ def test_daily_crontab():
         days_of_month=[],
         months=[],
         days_of_week=[Weekday.MONDAY, Weekday.WEDNESDAY],
-        frequency=CronFrequency.DAY,
+        frequency=JobFrequency.DAY,
+    )
+
+    created_job = set_crontab(config=crontab, write=False)
+    assert created_job.frequency(year=2023) == 104
+
+
+def test_daily_crontab_order_of_days():
+    crontab = CrontabFrequency(
+        hour=12,
+        days_of_month=[],
+        months=[],
+        days_of_week=[Weekday.WEDNESDAY, Weekday.MONDAY],
+        frequency=JobFrequency.DAY,
     )
 
     created_job = set_crontab(config=crontab, write=False)
@@ -26,8 +39,21 @@ def test_monthly_crontab():
         days_of_month=[4],
         months=[Month.JANUARY, Month.APRIL, Month.JULY, Month.OCTOBER],
         days_of_week=[],
-        frequency=CronFrequency.MONTH,
+        frequency=JobFrequency.MONTH,
     )
 
     created_job = set_crontab(config=crontab, write=False)
     assert created_job.frequency() == 4
+
+
+def test_monthly_crontab_order_of_months():
+    crontab = CrontabFrequency(
+        hour=12,
+        days_of_month=[8, 4],
+        months=[Month.OCTOBER, Month.JULY, Month.APRIL, Month.JANUARY],
+        days_of_week=[],
+        frequency=JobFrequency.MONTH,
+    )
+
+    created_job = set_crontab(config=crontab, write=False)
+    assert created_job.frequency() == 8
