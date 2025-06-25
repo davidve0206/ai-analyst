d8125621f330a25d78d2dc70df4704340ee67a11
davidve0206 <davidve0206@gmail.com>
2025-06-25 20:29:09 +0100

    feat: add capability to convert markdown to PDF

--- src/agents/utils/output_utils.py
+++ src/agents/utils/output_utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from pathlib import Path
+from markdown_pdf import MarkdownPdf, Section
 from semantic_kernel.agents import Agent
 from semantic_kernel.contents import FunctionCallContent, FunctionResultContent
 from semantic_kernel.contents.chat_message_content import ChatMessageContent
@@ -48,12 +50,34 @@ async def invoke_agent_in_chat_mode(agent: Agent):
         invoke_agent_displaying_intermediate_steps(agent, user_input)
 
 
-def store_response_with_timestamp(response: str, file_name: str):
+def store_response_with_timestamp(response: str, file_name: str) -> Path:
     """
     Store the agent's response in a file.
     """
     timestamp = datetime.now().strftime("%y%m%d%H%M")
-    file_path = BASE_DIR / "outputs" / f"{timestamp}-{file_name}"
+    file_path = BASE_DIR / "outputs" / f"{timestamp}-{file_name}.md"
 
     with open(file_path, "a") as file:
         file.write(response)
+
+    return file_path
+
+
+def convert_markdown_to_pdf(markdown_path: Path) -> Path:
+    """
+    Convert the given markdown content to a PDF file.
+    """
+    if not markdown_path.exists() or not markdown_path.is_file():
+        raise FileNotFoundError(f"The file {markdown_path} does not exist.")
+
+    markdown_content = None
+    with open(markdown_path, "r") as file:
+        markdown_content = file.read()
+
+    # By default, no table of content is generated in the PDF.
+    pdf = MarkdownPdf(toc_level=0)
+    pdf_path = markdown_path.with_suffix(".pdf")
+    pdf.add_section(Section(text=markdown_content, toc=False))
+    pdf.save(pdf_path)
+
+    return pdf_path
