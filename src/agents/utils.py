5a6741cc53ac8e47cd9f8951943df087cd1aa5a8
davidve0206 <davidve0206@gmail.com>
2025-07-08 21:09:47 +0100

    feat: add utility to get temp files as parts for the llm

--- src/agents/utils.py
+++ src/agents/utils.py
@@ -1,4 +1,6 @@
+import base64
 from enum import Enum
+from pathlib import Path
 
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import (
@@ -72,3 +74,51 @@ def extract_graph_response_content(response: dict) -> str:
 
     last_message = messages[-1]
     return last_message.content
+
+
+def create_prompt_parts_from_file_list(file_list: list[Path]) -> list[dict]:
+    """
+    Create a list of parts from a list of file names.
+
+    Args:
+        file_list (list[str]): List of file names.
+
+    Returns:
+        list[dict]: List of parts with file names.
+    """
+    prompt_parts = []
+    for file in file_list:
+        if file.is_file():
+            if file.suffix.lower() == ".png":
+                prompt_parts.append(
+                    {
+                        "type": "text",
+                        "text": f"Contents of file {file.name}:",
+                    }
+                )
+                prompt_parts.append(
+                    {
+                        "type": "image",
+                        "source_type": "base64",
+                        "mime_type": "image/png",  # or image/png, etc.
+                        "data": base64.b64encode(file.read_bytes()).decode(
+                            "utf-8"
+                        ),  # Encode the file content to base64
+                    }
+                )
+            elif file.suffix.lower() == ".csv":
+                prompt_parts.append(
+                    {
+                        "type": "text",
+                        "text": f"Contents of file {file.name}: \n\n {file.read_text()}",
+                    }
+                )
+        else:
+            prompt_parts.append(
+                {
+                    "type": "text",
+                    "text": f"File {file.name} was not found.",
+                }
+            )
+
+    return prompt_parts
--- tests/agents/test_utils.py
+++ tests/agents/test_utils.py
@@ -2,6 +2,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 from src.agents.utils import PrompTypes
 from src.configuration.settings import BASE_DIR
+from .helpers import test_temp_dir
 
 
 def test_render_system_prompt_template(monkeypatch):
@@ -46,3 +47,26 @@ def test_render_human_prompt_template(monkeypatch):
         rendered_template.content
         == "You are a translator and your role is to translate the text sent by the user to spanish"
     )
+
+
+def test_create_prompt_parts_from_file_list():
+    # Import just for this test to avoid overriding monkeypatches on other tests
+    from src.agents.utils import create_prompt_parts_from_file_list
+
+    csv_file_name = "sales_analysis_Spain_sales_fixture.csv"
+    png_file_name = "sales_projection_spain_fixture.png"
+    file_list = [
+        test_temp_dir / csv_file_name,
+        test_temp_dir / png_file_name,
+    ]
+    parts = create_prompt_parts_from_file_list(file_list)
+
+    assert len(parts) == 3
+    assert parts[0]["type"] == "text"
+    assert csv_file_name in parts[0]["text"]
+    assert parts[1]["type"] == "text"
+    assert png_file_name in parts[1]["text"]
+    assert parts[2]["type"] == "image"
+    assert parts[2]["source_type"] == "base64"
+    assert parts[2]["mime_type"] == "image/png"
+    assert parts[2]["data"]  # make sure data is not empty
