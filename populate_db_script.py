4dd9f0a201d819a9ed4c401ed3ae4aa14a158ca7
davidve0206 <davidve0206@gmail.com>
2025-08-01 16:57:59 +0100

    chore: rename default_db_script to populate_db_script for clarity

--- populate_db_script.py
+++ populate_db_script.py
@@ -1,16 +1,13 @@
 """Script to populate the default database with initial data."""
 
-from src.configuration.db import (
+from src.configuration.db_service import (
     KpiPeriodsEnum,
-    KpiDirectionsEnum,
     SalesGroupingsEnum,
     default_config_db_sessionmaker,
 )
 from src.configuration.recipients import add_recipient_email
-from src.configuration.kpis import (
-    KpiRequest,
+from src.configuration.db_models import (
     SalesReportRequest,
-    add_kpi_request,
     add_sales_report_request,
 )
 
@@ -21,12 +18,6 @@ def populate_default_db():
 
     # Add default recipient emails
     recipient_emails = ["test1@test.com", "test2@test.com"]
-    kpi = KpiRequest(
-        name="Gross Profit Margin",
-        description="The percentage of revenue that exceeds the cost of the goods sold; it might be referred as profit in the Sales table.",
-        direction=KpiDirectionsEnum.HIGHER.value,
-        period=KpiPeriodsEnum.QUARTERLY.value,
-    )
     sales = SalesReportRequest(
         grouping=SalesGroupingsEnum.STATE.value,
         grouping_value="California",
@@ -36,7 +27,6 @@ def populate_default_db():
     for email in recipient_emails:
         add_recipient_email(session_maker, email)
 
-    add_kpi_request(session_maker, kpi)
     add_sales_report_request(session_maker, sales)
 
 
